121225366dd889f699b6d0c9b9de971f1c45c00a
RemakePower <diandaokui@qq.com>
2023-05-30 21:33:34 +0800

    fix register

--- src/engine/server/register.cpp
+++ src/engine/server/register.cpp
@@ -99,8 +99,8 @@ void CRegister::Init(CNetServer *pNetServer, IEngineMasterServer *pMasterServer,
 
 void CRegister::RegisterUpdate(int Nettype)
 {
-	int64 Now = time_get();
-	int64 Freq = time_freq();
+	int64_t Now = time_get();
+	int64_t Freq = time_freq();
 
 	if(!g_Config.m_SvRegister)
 		return;
@@ -239,13 +239,9 @@ int CRegister::RegisterProcessPacket(CNetChunk *pPacket)
 {
 	// check for masterserver address
 	bool Valid = false;
-	NETADDR Addr1 = pPacket->m_Address;
-	Addr1.port = 0;
-	for(int i = 0; i < IMasterServer::MAX_MASTERSERVERS; i++)
+	for(auto &MasterserverInfo : m_aMasterserverInfo)
 	{
-		NETADDR Addr2 = m_aMasterserverInfo[i].m_Addr;
-		Addr2.port = 0;
-		if(net_addr_comp(&Addr1, &Addr2) == 0)
+		if(net_addr_comp_noport(&pPacket->m_Address, &MasterserverInfo.m_Addr) == 0)
 		{
 			Valid = true;
 			break;
